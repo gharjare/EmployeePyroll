3f676fcd64ca6cd53ec0f65512a623c2864adc03
gharjare <gharjarechetan@gmail.com>
2023-03-04 15:37:08 +0530

    [chetan gharjre]ER digram

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -41,6 +41,58 @@ select count(salary) as Number_Of_Male_Employee from employee_pyroll where Gende
 select count(salary) as Number_Of_Female_Employee from employee_pyroll where Gender = 'F' GROUP BY Gender
 select count(salary) as Total_Employee from employee_pyroll
 --UC8
+alter table employee_pyroll add PhoneNumber varchar(250);
+alter table employee_pyroll add address varchar(250);
+alter table employee_pyroll add department varchar(150);
+--uc9
+alter table employee_pyroll add  basic_pay int;
+alter table employee_pyroll add deduction int;
+alter table employee_pyroll add taxable_pay int;
+alter table employee_pyroll add income_tax int;
+alter table employee_pyroll add net_pay int;
+--10
+update employee_pyroll
+set department = 'Sales'
+where name = 'Terisa'
+insert into employee_pyroll (name,salary,start,Gender,PhoneNumber,address,department,deduction,basic_pay,taxable_pay,income_tax,net_pay) values ('Teresa', 20000,'2022-08-06','F',7936462332,'America','Marketing',10000,20000,20000,50000,15000);
+--UC11
 
+create table EmployeePyroll
+(empid int primary key identity(1,1),
+Name varchar (100),
+PhoneNumber bigint,
+Address varchar (100)
+)
+select * from EmployeePyroll
+insert into EmployeePyroll(Name,PhoneNumber,Address) values ('Chetan',7038860320,'Maharashtra')
+insert into EmployeePyroll(Name,PhoneNumber,Address) values ('Rahul',7487222510,'Delhi')
+insert into EmployeePyroll(Name,PhoneNumber,Address) values ('Vishal',7298415510,'Jammu')
+insert into EmployeePyroll(Name,PhoneNumber,Address) values ('Kartik',9822645240,'Sikkim')
+create table Department
+(Did int primary key identity(1,1),
+DepartName varchar(50),
+id int not null,
+foreign key (id) REFERENCES EmployeePyroll(empid)
+)
+select * from Department
 
-
+insert into Department(DepartName,id) values ('Production',1)
+insert into Department(DepartName,id) values ('Marketing',2)
+insert into Department(DepartName,id) values ('Sales',3)
+create table Salary
+(Sid int primary key identity(1,1),
+Salary bigint,
+BasicPay bigint,
+Deduction bigint,
+TaxaBlePay bigint,
+IncomeTax bigint,
+NetPay bigint,
+id int not null,
+foreign key (id) REFERENCES EmployeePyroll (empid)
+)
+select * from Salary
+insert into Salary(Salary,BasicPay,Deduction,TaxaBlePay,IncomeTax,NetPay,id) values (50000,45000,50000,3000,5000,60000,1)
+insert into Salary values (60000,55000,40000,4000,6000,70000,2)
+insert into Salary values (70000,60000,30000,3000,7000,80000,3)
+insert into Salary values (60000,55000,40000,4000,6000,70000,4)
+select * from EmployeePyroll,Department where EmployeePyroll.empid = Department.id and EmployeePyroll.empid=1;
